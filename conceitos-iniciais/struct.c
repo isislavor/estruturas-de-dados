4f6d7e96986ea89e0a94f8feebe27faafbb8eef2
Isis Lavor <isisnascimento142@gmail.com>
2025-02-22 22:38:37 -0300

    Registro(Struct) simples

--- conceitos-iniciais/struct.c
+++ conceitos-iniciais/struct.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+struct Cachorro {
+    int idade;
+    float peso;
+    char nome[50];
+};
+
 int main() {
-    printf("Hello world!\n");
+    struct Cachorro Cachorro1 = {2,15.3,"Bob"};
+    printf("%d\n", Cachorro1.idade);
+    printf("%f\n",Cachorro1.peso);
+    printf("%s\n",Cachorro1.nome);
     return 0;
 }; 
